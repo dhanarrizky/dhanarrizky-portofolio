90aa15ff63edabde8b86bb86e041f04a68b0a6f7
dhanarrizky <dhanarrizky23@gmail.com>
2024-07-19 15:00:57 +0700

    complating code for navbar, just need lil code again for fix it

---
+++ static/js/script.js
@@ -0,0 +1,34 @@
+// styleing for navbar
+const navLinks = document.querySelectorAll('.nav-item--links');
+const bars = document.querySelector('.bars');
+const sidebar = document.querySelector('.side-bar');
+
+bars.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        const href = link.getAttribute('href');
+
+        navLinks.forEach(l => {
+            if(l.getAttribute('href') === href) {
+                l.parentElement.classList.add('active');
+            } else {
+                l.parentElement.classList.remove('active');
+            }
+        })
+
+        sidebar.classList.remove('active');
+    });
+});
+
+const checkWidth = () => {
+    if(window.innerWidth >= 1102){
+        sidebar.classList.remove('active');
+    }
+    console.log("call checkWidth function");
+};
+
+window.addEventListener('resize', checkWidth);
+checkWidth();
